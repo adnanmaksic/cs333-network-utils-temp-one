30498acf1fcf99411ced42b95a6f9bb9fc392c03
nathanmarquis659 <nathanmarquis659@u.boisestate.edu>
2026-03-25 10:48:15 -0600

    baseline python script

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+class Packet:
+    def __init__(self, source_ip, dest_ip, payload):
+        self.source_ip = source_ip
+        self.dest_ip = dest_ip
+        self.payload = payload
+
+    def __str__(self):
+        return f"Packet from {self.source_ip} to {self.dest_ip} with payload: {self.payload}"
